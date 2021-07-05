857445a312bc64fc5736e484f0df0ac923d98aba
Michael Canziani <canziani.michael@gmail.com>
2021-07-05 19:40:31 -0400

    Added Vuetify package to client

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -4,7 +4,6 @@
 
 <script>
 import Navbar from "@/components/Navbar";
-
 export default {
   name: 'App',
   components: {
--- src/client/src/main.js
+++ src/client/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import vuetify from './plugins/vuetify'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
